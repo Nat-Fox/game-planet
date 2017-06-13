6461b05821cb3697c9127d025b697cc26d217d41
Natalia Garrido <natgramirez@outlook.com>
2017-06-13 12:07:06 -0400

    dando estilos generales

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
-// // JS Nat para validar el formulario de contacto
-// var botonEnviar = document.getElementById('enviar');
-// botonEnviar.addEventListener('click', validarContact);
+// JS Nat para validar el formulario de contacto
+var botonEnviar = document.getElementById('enviar');
+botonEnviar.addEventListener('click', validarContact);
 
-// function validarContact() {
-//     // Obtengo los valores del formulario
-//     var name = document.getElementById('name');
-//     var email = document.getElementById('email');
-//     var message = document.getElementById('message');
+function validarContact() {
+    // Obtengo los valores del formulario
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var message = document.getElementById('message');
 
 
-//     // Si los value vienen vacios envio mensaje
-//     if (name.value === '') {
+    // Si los value vienen vacios envio mensaje
+    if (name.value === '') {
 
-//         name.setAttribute('placeholder', '● Please enter your name');
-//     }
-//     if (email.value === '') {
-//         email.setAttribute('placeholder', '● Please enter your email');
-//     }
-//     if (message.value === '') {
-//         message.setAttribute('placeholder', '● Please enter your message')
-//     }
+        name.setAttribute('placeholder', '● Please enter your name');
+    }
+    if (email.value === '') {
+        email.setAttribute('placeholder', '● Please enter your email');
+    }
+    if (message.value === '') {
+        message.setAttribute('placeholder', '● Please enter your message')
+    }
 
-//     if (name.value !== '' && email.value !== '' && message.value !== '') {
-//         botonEnviar.setAttribute('type', 'submit');
-//     }
-// }
+    if (name.value !== '' && email.value !== '' && message.value !== '') {
+        botonEnviar.setAttribute('type', 'submit');
+    }
+}
